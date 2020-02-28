1c05c75b097f672a5640b2d2dc2b560a102cd187
David Melendez <dave_melendez@hotmail.com>
2020-02-28 14:24:45 -0800

    WeatherForecast API w/ swagger

---
+++ 2020-02-29-dotnet-openapi-3-swashbuckle-oneof/OpenApiWebAppExample/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace OpenApiWebAppExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Simple API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://elcamino.cloud/privacy.html"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Dave Melendez",
+                        Email = string.Empty,
+                        Url = new Uri("https://elcamino.cloud"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url =
+                            new Uri("https://github.com/dlmelendez/elcamino-cloud-blog-companion/blob/master/LICENSE"),
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple API");
+                c.RoutePrefix = string.Empty; // This puts the swagger UI at the root of the web app
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ 2020-02-29-dotnet-openapi-3-swashbuckle-oneof/README.md
@@ -0,0 +1,1 @@
+# Code example that supports blog entry for 2020-02-29
